f4f7e741f57ee1d1fcab6227c46e2366d36f6dc9
chmarguerite <chmarguerite@testlogiciel.pro>
2019-12-20 15:52:42 +0100

    grid

--- hub/lancerFF_Node.bat
+++ hub/lancerFF_Node.bat
@@ -1,1 +1,1 @@
-java -jar selenium-server-standalone-3.141.59.jar -role node -hub http://localhost:4444/grid/register -nodeConfig nodeConfig.json
+java -Dwebdriver.chrome.driver=chromedriver.exe -jar selenium-server-standalone-3.141.59.jar  -role node -hub  http://localhost:5555/wd/hub -browser "browserName=chrome"
